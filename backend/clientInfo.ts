52023e2c576e590af2c99873e78cf90c88183c59
FunScript1 <101437126+FunScript1@users.noreply.github.com>
2022-07-12 23:36:55 +0300

    Update clientInfo.ts

--- backend/clientInfo.ts
+++ backend/clientInfo.ts
@@ -2,7 +2,7 @@ import { Socket } from "socket.io"
 
 export default class ClientInfo {
   public latency = 0
-  public name = "Unnamed"
+  public name = "Fun"
   public isHost = false
   public loggedIn = false
   public trackUri = ""
